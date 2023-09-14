1be4348a5704f1d1eab7ea0fb93da121115c945c
matheus <de.leite2014@gmail.com>
2023-09-14 17:25:11 -0300

    feat: Completed the responsiveness and navigation logic of the Crew page
    In this commit, I implemented the following improvements to the Crew page:
    
    - Responsiveness:
       - Adapted the page layout and elements to work efficiently on different screen sizes, including mobile and desktop devices.
       - I used media queries to adjust the design as needed.
       - Tested on multiple resolutions to ensure a consistent user experience.
    
    - Navigation Logic:
       - I implemented navigation between each crew member's information, allowing users to click on buttons to smoothly scroll to the desired information.
       - I added a navigation bar with input type radio.
       - Included the navigation bar responsive navigation for mobile devices.

--- src/components/CrewNav/index.tsx
+++ src/components/CrewNav/index.tsx
@@ -1,31 +1,69 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import * as S from './styled';
-import DouglasImg from "../../assets/crew/image-douglas-hurley.png";
-import Radio from '../Form/Radio';
+import * as S from "./styled";
+import Radio from "../Form/Radio";
+import douglasImg from "../../assets/crew/image-douglas-hurley.png";
+import markImg from "../../assets/crew/image-mark-shuttleworth.png";
+import victorImg from "../../assets/crew/image-victor-glover.png";
+import anoushehImg from "../../assets/crew/image-anousheh-ansari.png";
 
 
+const creewInfo = [
+  {
+    subtitle: "Commander",
+    title: "Douglas Hurley",
+    paragraph:
+      "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+  },
+  {
+    subtitle: "Mission Specialist",
+    title: "MARK SHUTTLEWORTH",
+    paragraph:
+      "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist..",
+  },
+  {
+    subtitle: "PILOT",
+    title: "Victor Glover",
+    paragraph:
+      "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+  },
+  {
+    subtitle: "Flight Engineer",
+    title: "Anousheh Ansari",
+    paragraph:
+      "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+  },
+];
 
 const CrewNav = () => {
-  const [value,setValue] = React.useState('Commander');
+  const professionArr = creewInfo.map(({ subtitle }) => subtitle);
+  const [value, setValue] = React.useState(professionArr[0]);
+  const creewInfoFilter = creewInfo.filter(
+    ({ subtitle }) => subtitle === value,
+  );
 
   return (
     <S.Container>
       <S.InfoContainer>
-        <div>
-        <S.SubTitle>Commander</S.SubTitle>
-        <S.Title>Douglas Hurley</S.Title>
-        <S.Paragraph>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</S.Paragraph>
-        </div>
+        {creewInfoFilter.map(({ subtitle, title, paragraph }) => (
+          <div key={subtitle}>
+            <S.SubTitle>{subtitle}</S.SubTitle>
+            <S.Title>{title}</S.Title>
+            <S.Paragraph>{paragraph}</S.Paragraph>
+          </div>
+        ))}
         <S.NavInput>
-          <Radio options={["Commander", "Mission Specialist", "PILOT", "Flight Engineer"]} value={value} setValue={setValue}/>
-        </S.NavInput>      
+          <Radio options={professionArr} value={value} setValue={setValue} />
+        </S.NavInput>
       </S.InfoContainer>
       <S.ImgContainer>
-        <Image src={DouglasImg} alt='image-douglas-hurle'/>
+        {value === professionArr[0] &&  <Image src={douglasImg} alt="image-douglas-hurle" />}
+        {value === professionArr[1] &&  <Image src={markImg} alt="image-mark-shuttleworth" />}
+        {value === professionArr[2] &&  <Image src={victorImg} alt="image-victor-glover" />}
+        {value === professionArr[3] &&  <Image src={anoushehImg} alt="image-anousheh-ansari" />}
       </S.ImgContainer>
     </S.Container>
-  )
-}
+  );
+};
 
-export default CrewNav
+export default CrewNav;
--- src/components/CrewNav/styled.ts
+++ src/components/CrewNav/styled.ts
@@ -5,8 +5,17 @@ import { theme } from "../../styles/theme";
 export const Container = styled.div`
   position: relative;
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+    flex-direction: column;
+    text-align: center;
+    gap: 22px;
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}){
+    flex-direction: column-reverse;
+  }
 `;
 
 export const InfoContainer = styled.div`
@@ -14,6 +23,16 @@ export const InfoContainer = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: 450px;
   align-items: center;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+    grid-template-rows: auto;
+    gap: 40px;
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}) {
+   display: flex;
+   flex-direction: column-reverse;
+   align-items: center;
+   gap: 32px;
+  }
 `;
 
 export const SubTitle = styled.span`
@@ -22,8 +41,13 @@ export const SubTitle = styled.span`
   font-size: ${theme.font.size.size_32px};
   line-height: normal;
   text-transform: uppercase;
-  margin-bottom: 15px;
   opacity: 0.5042;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+    font-size: ${theme.font.size.size_24px};
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}) {
+    font-size: ${theme.font.size.size_16px};
+  }
 `;
 
 export const Title = styled.h1`
@@ -33,6 +57,15 @@ export const Title = styled.h1`
   line-height: normal;
   text-transform: uppercase;
   margin-bottom: 27px;
+  margin-top: 15px;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+    margin-bottom: 16px;
+    margin-top: 8px;
+    font-size: ${theme.font.size.size_40px};
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}) {
+    font-size: ${theme.font.size.size_24px};
+  }
 `;
 
 export const Paragraph = styled.p`
@@ -41,12 +74,36 @@ export const Paragraph = styled.p`
   font-family: ${theme.font.family.barlow};
   font-size: ${theme.font.size.size_18px};
   line-height: 32px;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+    max-width: 530px;
+    font-size: ${theme.font.size.size_16px};
+    line-height: 28px;
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}) {
+    font-size: ${theme.font.size.size_15px};
+  }
 `;
 
 export const ImgContainer = styled.div`
   position: absolute;
   top: -65px;
   right: 0;
+  @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+   position: static;
+    img {
+      width: 400px;
+      height: 500px;
+    }
+  }
+  @media screen and (max-width: ${theme.font.responsiveDevice.w760}){
+    width: 100%;
+    border-bottom: 1px solid #383B4B;
+    img {
+      width: 177px;
+      height: 222px;
+      margin: 0 auto;
+    }
+  }
 `;
 
 export const NavInput = styled.div`
@@ -60,13 +117,18 @@ export const NavInput = styled.div`
       display: inline-block;
       width: 15px;
       height: 15px;
-      padding: 5px;
       margin-right: 24px;
       border-radius: 50%;
       background-color: #fff;
       opacity: 0.1744;
       cursor: pointer;
       transition: 0.2s;
+      @media screen and (max-width: ${theme.font.responsiveDevice.w1024}) {
+        width: 10px;
+        height: 10px;
+        margin-right: 16px;
+
+    }
     }
 
     &:hover::before {
@@ -78,4 +140,5 @@ export const NavInput = styled.div`
       opacity: 1;
     }
   }
+  
 `;
